7936cdb4dc542b915c225cc863f2e0caf1396848
William Navarre <william.navarre@utoronto.ca>
2018-08-16 20:49:40 -0400

    first pass decided to use dpylr module to start

--- Assignment3.R
+++ Assignment3.R
@@ -1,3 +1,12 @@
 ## Assignment 3 for R Programming Course
 
-print("Hello World!")
+## Will open 
+
+best <- function(state, outcome) {
+        library(dplyr)
+        outcome_data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
+        state_subset <<- filter(outcome_data, State == state) 
+}
+## Check that state and outcome are valid
+## Return hospital name in that state with lowest 30-day death
+## rate
