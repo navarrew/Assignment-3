63173b4bb5b9bdd3e00e1ab115372a7736eb9250
William Navarre <william.navarre@utoronto.ca>
2018-08-19 17:40:09 -0400

    fixed problem with data frame

--- Assignment3.R
+++ Assignment3.R
@@ -6,24 +6,25 @@
 
 best <- function(state, outcome) {
         library(dplyr)
-#        if (outcome == "heart attack") {outcome_col <-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"}
-#        else if(outcome == "heart failure") {outcome_col <-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"}
-#        else if(outcome == "pneumoniae") {outcome_col <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"}
- #       else print("Not valid")
+        #        if (outcome == "heart attack") {outcome_col <-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"}
+        #        else if(outcome == "heart failure") {outcome_col <-"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"}
+        #        else if(outcome == "pneumoniae") {outcome_col <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"}
+        #       else print("Not valid")
         
         if (outcome == "heart attack") {outcome_col <- 11}
         else if(outcome == "heart failure") {outcome_col <-17}
         else if(outcome == "pneumoniae") {outcome_col <- 23}
         else print("Not valid")
-
+        
         outcome_data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
         outcome_data_trimmed <- outcome_data[,c(2,7,outcome_col)]
         state_subset <<- filter(outcome_data_trimmed, State == state)
-        good <<- complete.cases(state_subset)
-        final_frame <<- state_subset[good,]
-        final_frame[ ,3] <<- as.numeric(final_frame[ ,3])
+        state_subset[ ,3] <<- as.numeric(state_subset[ ,3])
+        final_frame <<- state_subset[complete.cases(state_subset),]
+##        final_frame <<- state_subset[good,]
+##        final_frame[ ,3] <<- as.numeric(final_frame[ ,3])
         str(final_frame)
-
+        
         
 }
 ## Check that state and outcome are validrm
